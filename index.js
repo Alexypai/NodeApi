db0e6d8eb2e4b2cf15f0a7df534212b1645b8866
Alexy <alexy.paiva@ipilyon.net>
2023-05-03 10:42:35 +0200

    git ignor + code get Marine

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Route GET pour la page "contact"
+app.get('/Marine', (req, res) => {
+    res.send('Contactez-nous Marine la plus forte  !');
+});
+
 app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
