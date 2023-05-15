6e44773993d0c4b98670d9d19eeff06bd0517242
Alexy <alexy.paiva@ipilyon.net>
2023-05-15 16:28:22 +0200

    Finalisation du chatBot méteo

--- dialogflow.js
+++ dialogflow.js
@@ -1,14 +1,16 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const uuid = require('uuid');
-const axios = require("axios");
-const moment = require('moment');
 
+const config = require('../../config/config')
+const {getAnimeList} = require("./webhook/animeService");
+const {getWeatherResponse} = require("./webhook/weatherService");
 
-const projectId = 'ipiagent1-anpv';
+const projectId = config.projectId;
 const sessionClient = new dialogflow.SessionsClient();
 
-function handleChatRequest(req, res) {
-    const sessionId = uuid.v4();
+async function handleChatRequest(req, res) {
+
+    const sessionId = "sessionChatBot69";
     const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
     const message = req.body.message;
 
@@ -21,50 +23,27 @@ function handleChatRequest(req, res) {
             },
         },
     };
-
-    sessionClient.detectIntent(request).then((responses) => {
+    sessionClient.detectIntent(request).then(async (responses) => {
         const result = responses[0].queryResult;
-        if (result.action.includes("weather")){
-            const dateDialogFlow = result.parameters.fields.date.stringValue;
-            const address = result.parameters.fields.address.stringValue;
-            if(dateDialogFlow.length > 0  && address.length > 0){
-                let weatherTarget = null;
-                const inputFormatedDate = moment(dateDialogFlow).format('YYYY-MM-DD')
-                axios.get('https://api.openweathermap.org/data/2.5/forecast', {
-                    params: {
-                        appid: '16e409ca55174b0489510aa4f6a9e467',
-                        q: address,
-                    }
-                })
-                    .then(response => {
-                       /* res.send(response.data.list);*/
-                        // Parcourir toutes les listes
-                        response.data.list.forEach(weatherList => {
-                            console.log(inputFormatedDate);
-                                // Vérifier si la variable "dt_txt" contient la date recherchée
-                                const dateWeather = moment(weatherList.dt_txt).format('YYYY-MM-DD');
-                                console.log(dateWeather);
-                                if (dateWeather === inputFormatedDate) {
-                                    weatherTarget = weatherList;
-                                }
-                            return weatherTarget;
-                        });
-                        if(weatherTarget){
-                            res.send(weatherTarget);
-                        }else {
-                            res.send("ERREUR DATE NON RETROUVER");
-                        }                    })
-                    .catch(error => {
-                        console.log(error);
-                        res.send(error.data);
-                    });
-            }else{
-                res.send(result);
-            }
-        }else{
-            res.send({ message: "No weather"});
+        let chatBotResult = null;
+        console.log(result.intent.displayName)
+        switch (true) {
+            case (result.intent.displayName.includes("weather")):
+                chatBotResult = await getWeatherResponse(result);
+                break;
+            case (result.intent.displayName.includes("anime")):
+                chatBotResult = await getAnimeList();
+                break;
+            case (result.intent.displayName === ""):
+                chatBotResult = "Désolé, je n'ai compris ce que vous me demandé.\n Assurez-vous de me demander un contexte de météo ou d'animés"
+                break;
         }
+        console.log(chatBotResult)
+        res.send(chatBotResult);
     });
 }
 
 module.exports = { handleChatRequest };
+
+
+
